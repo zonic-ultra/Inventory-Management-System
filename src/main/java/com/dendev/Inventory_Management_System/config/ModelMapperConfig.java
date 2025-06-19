9f0bb14b0d7e65c4be38d2a330d61feb5c95877f
zonic-ultra <zonicultra@gmail.com>
2025-06-19 19:41:47 +0700

    for database

---
+++ src/main/java/com/dendev/Inventory_Management_System/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package com.dendev.Inventory_Management_System.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
+
+}
