64d750cd31d2ba26d5454a614adf8914035f4a16
zonic-ultra <zonicultra@gmail.com>
2025-06-22 13:33:05 +0700

    config

--- src/main/java/com/dendev/Inventory_Management_System/config/ModelMapperConfig.java
+++ src/main/java/com/dendev/Inventory_Management_System/config/ModelMapperConfig.java
@@ -2,8 +2,6 @@ package com.dendev.Inventory_Management_System.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
